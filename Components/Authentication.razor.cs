ac230539d380b97259fab96598bf3c3acc3847e8
sish-siameh <sish@siameh.com>
2024-10-11 11:55:13 +0200

    add authentication

---
+++ Components/Authentication.razor.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Components;
+
+namespace FlexyBox.web.Components
+{
+    public partial class Authentication
+    {
+        [Parameter] public string Action { get; set; }
+    }
+}
--- Components/AuthorizedProfile.razor.cs
+++ Components/AuthorizedProfile.razor.cs
@@ -1,8 +1,24 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+
+
+
 namespace FlexyBox.web.Components
 {
     public partial class AuthorizedProfile
     {
+        [Parameter]
         public string UserProfilePicture { get; set; }
+
+        [Parameter]
         public string UserName { get; set; }
+
+        [Inject]
+        public NavigationManager Navigation { get; set; }
+
+        private async Task LogOut_ClickHandlerAsync(Microsoft.AspNetCore.Components.Web.MouseEventArgs e)
+        {
+            Navigation.NavigateToLogout("authentication/logout");
+        }
     }
 }
