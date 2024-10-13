e0650acc18d34f9c80b32b9f18c973cc212e845e
sish-siameh <sish@siameh.com>
2024-10-13 17:26:22 +0200

    map to desired port and also add waiting components

---
+++ Components/Waiting.razor.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Components;
+
+namespace FlexyBox.web.Components
+{
+    public partial class Waiting
+    {
+        [Parameter]
+        public string text { get; set; }
+    }
+}
